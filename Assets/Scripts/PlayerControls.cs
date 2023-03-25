fe4cf9c4701660ce8b95d985607e6210c9c27f89
Chyld989 <chyld989@gmail.com>
2023-03-25 11:29:57 -0500

    - Added pitch/yaw/roll rotation as ship moves on screen

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,13 @@ public class PlayerControls : MonoBehaviour {
 	[SerializeField] float MaxXLocation = 9f;
 	[SerializeField] float MinYLocation = -5f;
 	[SerializeField] float MaxYLocation = 6f;
+	[SerializeField] float PositionPitchFactor = 2.5f;
+	[SerializeField] float ControlPitchFactor = -10f;
+	[SerializeField] float PositionYawFactor = 2f;
+	[SerializeField] float ControlRollFactor = -15f;
+
+	float XThrow = 0f;
+	float YThrow = 0f;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -16,18 +23,45 @@ public class PlayerControls : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		var xThrow = Input.GetAxis("Horizontal");
-		var yThrow = Input.GetAxis("Vertical");
+		ProcessPlayerTranslation();
+		ProcessPlayerRotation();
+	}
 
-		var xOffset = xThrow * Time.deltaTime * PlayerSpeed;
-		var yOffset = yThrow * Time.deltaTime * PlayerSpeed;
+	private void ProcessPlayerTranslation() {
+		XThrow = Input.GetAxis("Horizontal");
+		YThrow = Input.GetAxis("Vertical");
+
+		var xOffset = XThrow * Time.deltaTime * PlayerSpeed;
+		var yOffset = YThrow * Time.deltaTime * PlayerSpeed;
 
 		var newXPosition = transform.localPosition.x + xOffset;
 		newXPosition = Mathf.Clamp(newXPosition, MinXLocation, MaxXLocation);
+
 		var newYPosition = transform.localPosition.y + yOffset;
 		newYPosition = Mathf.Clamp(newYPosition, MinYLocation, MaxYLocation);
-		var newZPosition = transform.localPosition.z;
 
-		transform.localPosition = new Vector3(newXPosition, newYPosition, newZPosition);
+		transform.localPosition = new Vector3(newXPosition, newYPosition, transform.localPosition.z);
+	}
+
+	private void ProcessPlayerRotation() {
+		var pitch = GetPitch();
+		var yaw = GetYaw();
+		var roll = GetRoll();
+
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+	}
+
+	private float GetPitch() {
+		var pitchDueToPosition = transform.localPosition.y * PositionPitchFactor;
+		var pitchDueToControlThrow = YThrow * ControlPitchFactor;
+		return pitchDueToPosition + pitchDueToControlThrow;
+	}
+
+	private float GetYaw() {
+		return transform.localPosition.x * PositionYawFactor;
+	}
+
+	private float GetRoll() {
+		return XThrow * ControlRollFactor;
 	}
 }
